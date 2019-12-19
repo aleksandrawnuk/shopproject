873b02b158322cdb5060dbcc12ba01aed8ea88a7
Aleksandra Wnuk <aleksandrawnuk0@gmail.com>
2019-12-19 19:00:02 +0100

    added get methods

--- server/db/index.js
+++ server/db/index.js
@@ -11,9 +11,64 @@ const pool = mysql.createPool({
 
 let shopdb = {};
 
-shopdb.all = () => {
+shopdb.allProducts = () => {
     return new Promise((resolve, reject) => {
-        pool.query('SELECT * FROM status', (err, results) => {
+        pool.query(`SELECT * FROM products`, (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
+};
+
+shopdb.oneProduct = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM products where id = ?`, [id], (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            return resolve(results[0]);
+        });
+    });
+};
+
+shopdb.allCategories = () => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM categories`, (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
+};
+
+shopdb.allOrders = () => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM orders`, (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
+};
+
+shopdb.oneOrderByStatus = (status_id) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM orders where status_id = ?`, [status_id], (err, results) => {
+            if(err) {
+                return reject(err);
+            }
+            return resolve(results);
+        });
+    });
+};
+
+shopdb.allStatus = () => {
+    return new Promise((resolve, reject) => {
+        pool.query(`SELECT * FROM status`, (err, results) => {
             if(err) {
                 return reject(err);
             }
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,14 +3,64 @@ const db = require('../db');
 
 const router = express.Router();
 
-router.get('/status', async (req, res, next) => {
+router.get('/products', async (req, res, next) => {
     try {
-        let results = await db.all();
+        let results = await db.allProducts();
         res.json(results);
     } catch(e) {
         console.log(e);
         res.sendStatus(500);
     }
-})
+});
+
+router.get('/products/:id', async (req, res, next) => {
+    try {
+        let results = await db.oneProduct(req.params.id);
+        res.json(results);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+router.get('/categories', async (req, res, next) => {
+    try {
+        let results = await db.allCategories();
+        res.json(results);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+router.get('/orders', async (req, res, next) => {
+    try {
+        let results = await db.allOrders();
+        res.json(results);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+router.get('/orders/status/:id', async (req, res, next) => {
+    try {
+        let results = await db.oneOrderByStatus(req.params.id);
+        res.json(results);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
+
+router.get('/status', async (req, res, next) => {
+    try {
+        let results = await db.allStatus();
+        res.json(results);
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+});
 
 module.exports = router;
