9aee60f5c17e5bae5fb5ad14c558fed0075a4149
kjawo <konjaworski@gmail.com>
2020-01-03 22:17:00 +0100

    Add error handling for PUT products

--- db/index.js
+++ db/index.js
@@ -95,12 +95,41 @@ shopdb.addProduct = async (prod) => {
 
 shopdb.updateProduct = (id, prod) => {
     return new Promise((resolve, reject) => {
-        let sql = `CALL addOrUpdateProduct(${id}, "${prod.prodName}", "${prod.prodDesc}", ${prod.prodPrice}, ${prod.prodWeight}, ${prod.catID});`
-        pool.query(sql, [id, prod.prodName, prod.prodDesc, prod.prodPrice, prod.prodWeight, prod.catID], (err, results) => {
+        let rules = {
+            prodName: 'required',
+            prodDesc: 'required',
+            prodPrice: 'min:0.01',
+            prodWeight: 'min:0.01',
+            catID: 'required'
+        };
+
+        let validation = new Validator(prod, rules);
+
+        if (validation.fails()) {
+            validation.errors.add("Correct format", constants.correctProductJSON);
+            throw (validation.errors)
+        }
+
+        let sqlCategoryID = `SELECT * FROM categories WHERE id = ${prod.catID};`
+        pool.query(sqlCategoryID, [], (err, results) => {
+            // console.log(results);
+            if (results[0] == null) {
+                return reject({
+                    Errors: {catID: ['Category does not exist']},
+                    "Correct format": constants.correctProductJSON
+                });
+            }
             if (err) {
                 return reject(err);
             }
-            return resolve(results);
+
+            let sql = `CALL addOrUpdateProduct(${id}, "${prod.prodName}", "${prod.prodDesc}", ${prod.prodPrice}, ${prod.prodWeight}, ${prod.catID});`
+            pool.query(sql, [id, prod.prodName, prod.prodDesc, prod.prodPrice, prod.prodWeight, prod.catID], (err, results) => {
+                if (err) {
+                    return reject(err);
+                }
+                return resolve(results);
+            });
         });
     });
 };
@@ -168,7 +197,7 @@ shopdb.addOrder = (order) => {
             });
 
             order.orderlines.productID.forEach(function (item) {
-                if(!_.includes(prodIDsFromDB, item)) {
+                if (!_.includes(prodIDsFromDB, item)) {
                     return reject({
                         Errors: {productID: [`productID ${item} does not exist`]},
                         "Correct format": constants.correctOrderJSON
